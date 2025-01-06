3507fa519b7645d43a2d3e4f4c09b4dd7821e054
Vitaly Kutuzov <raidzer93@gmail.com>
2025-01-06 15:13:08 +0300

    add postCSS and handlebars

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,29 @@
 import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import path from "path";
+
+const contextData = {
+  title: "Проект чат",
+  test: "test",
+};
 
 export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: path.resolve(__dirname, "src/partials"),
+      context: contextData,
+    }),
+  ],
   server: {
     host: "127.0.0.1",
     open: true,
     port: 3000,
   },
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    outDir: "./build",
+  },
+  publicDir: "./static",
 });
