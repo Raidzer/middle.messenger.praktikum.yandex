7c5fff94403592ccbb9886b03e2ce3c15bcd3a71
Виталий Кутузов <kutuzov@algont.ru>
2025-02-17 13:25:40 +0300

    fix vite build

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,7 @@ export default defineConfig({
   root: "src",
   plugins: [
     handlebars({
-      partialDirectory: [
-        path.resolve(__dirname, "components"),
-      ],
+      partialDirectory: [path.resolve(__dirname, "components")],
       context: contextData,
     }),
   ],
@@ -26,28 +24,6 @@ export default defineConfig({
     postcss: "../postcss.config.сjs",
   },
   build: {
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, "src/index.html"),
-        login: path.resolve(__dirname, "src/pages/login/login.html"),
-        changePassword: path.resolve(
-          __dirname,
-          "src/pages/changePassword/changePassword.html"
-        ),
-        changeProfile: path.resolve(
-          __dirname,
-          "src/pages/changeProfile/changeProfile.html"
-        ),
-        error404: path.resolve(__dirname, "src/pages/error404/error404.html"),
-        error500: path.resolve(__dirname, "src/pages/error500/error500.html"),
-        message: path.resolve(__dirname, "src/pages/message/message.html"),
-        register: path.resolve(__dirname, "src/pages/register/register.html"),
-        userProfile: path.resolve(
-          __dirname,
-          "src/pages/userProfile/userProfile.html"
-        ),
-      },
-    },
     outDir: "../build",
   },
   publicDir: "../static",
